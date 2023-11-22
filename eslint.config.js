654fca4b072b2f7513c56ff98786c8f7cbf576e3
MerlinAlex <merlinalex@qq.com>
2023-11-22 17:23:03 +0800

    feat: update deps

--- eslint.config.js
+++ eslint.config.js
@@ -1,5 +1,4 @@
 // @ts-check
-import styleMigrate from '@stylistic/eslint-plugin-migrate'
 import merlin from './dist/index.js'
 
 export default merlin(
@@ -15,15 +14,6 @@ export default merlin(
     files: ['src/**/*.ts'],
     rules: {
       'perfectionist/sort-objects': 'error',
-    },
-  },
-  {
-    files: ['src/configs/*.ts'],
-    plugins: {
-      'style-migrate': styleMigrate,
-    },
-    rules: {
-      'style-migrate/migrate': ['error', { namespaceTo: 'style' }],
     },
   },
 )
