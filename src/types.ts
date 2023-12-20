d8ab70e89869a584820d787198bf7863f13078e6
MerlinAlex <merlinalex@qq.com>
2023-12-20 11:20:10 +0800

    feat: update toml use eslint-plugin-toml

--- src/types.ts
+++ src/types.ts
@@ -113,13 +113,6 @@ export interface OptionsFormatters {
   html?: 'prettier' | boolean
 
   /**
-   * Enable formatting support for TOML.
-   *
-   * Currently only support dprint.
-   */
-  toml?: 'dprint' | boolean
-
-  /**
    * Enable formatting support for Markdown.
    *
    * Support both Prettier and dprint.
@@ -263,6 +256,13 @@ export interface OptionsConfig extends OptionsComponentExts {
   yaml?: boolean
 
   /**
+   * Enable TOML support.
+   *
+   * @default true
+   */
+  toml?: boolean
+
+  /**
    * Enable linting for **code snippets** in Markdown.
    *
    * For formatting Markdown content, enable also `formatters.markdown`.
@@ -329,6 +329,7 @@ export interface OptionsConfig extends OptionsComponentExts {
     jsonc?: FlatConfigItem['rules']
     markdown?: FlatConfigItem['rules']
     yaml?: FlatConfigItem['rules']
+    toml?: FlatConfigItem['rules']
     react?: FlatConfigItem['rules']
   }
 }
